3a2ed0ab68db9aa6d75473fd736495b37868683d
rexto <40545271+rexto@users.noreply.github.com>
2019-02-23 20:57:02 +0100

    Update p26.py

--- Project_Euler/python/p26.py
+++ Project_Euler/python/p26.py
@@ -1,51 +1,40 @@
+from decimal import *
 cycles=[]
 
 def main():
-    #print (max(len_cycle(i)for i in range(2,1000)))
     i_new=0
     max_v_tuple =(0,"")
 
-#    for i in range(2,1000):
-#        if max(len_cycle(i), default=0)> max_v_tuple:
-#            max_v_tuple=max(len_cycle(i), default=0)
-#            i_new = i
-#
     for i in range(2,1000):
         if max(len_cycle(i),default=(0,""))>max_v_tuple:
             max_v_tuple = max(len_cycle(i),default=0)
             i_new = i
 
-    print(max_v_tuple, i_new)
+    print('cycle: {}\nlen:{}\ndivisor: {} '.format(max_v_tuple[1], max_v_tuple[0], i_new))
 
 
 
 
 def len_cycle(div):
-    num = str(1/div)[2:]
+    getcontext().prec=2000
+ 
+    num = str(1/Decimal(div))[2:]
+    
     
     global cycles
     for i in range(len(num)):
         cycles.clear()
+        skip_div=None
         for j in range(i+1,int(len(num[i:])/2)):
-            if num[i:j+1] == num[j+1:(j+1)*2]and not has_smaller_cycles(num[i:j+1]):
-                yield (len(num[i:j+1]),num[i:j+1])
+            if num[i:j+1] == num[j+1:(j+1)*2]:
 
-    
-#    return max((len(num[i:j+1])for i in range(len(num))
-#        for j in range(i+1,int(len(num[i:])/2)) 
-#        if num[i:j+1] == num[j+1:(j+1)*2]and not has_smaller_cycles(num[i:j+1])),default=0) 
-#
-   
-    
+                if num[i:int((j+1)/2)] == num[int((j+1)/2):j+1] and skip_div == None:
+                    skip_div=len(num[i:int((j+1)/2)])
 
+                if skip_div != None and len(num[i:j+1])%skip_div == 0:
+                    continue
                 
-def has_smaller_cycles(new_cycle):
-    global cycles
-    if not any(c for c in cycles if c in new_cycle):
-        cycles.append(new_cycle) 
-        return False
-    else:
-        return True
+                yield (len(num[i:j+1]),num[i:j+1])
 
 
 if __name__ == '__main__':
